995b87abc810be5bd31a24d5e7b8ffa1521a3307
heisose <ikiakheledestiny@gmail.com>
2025-07-07 11:54:17 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,236 @@
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1" />	 	 
+<meta content="IE=Edge" http-equiv="X-UA-Compatible" />
+<meta name="msvalidate.01" content="8BAB7574F2CFB135874093D7FC22CA9A" />
+<title>INDEX.JS: DOWNLOAD - CDNPKG</title>
+<meta name="description" content="Download index.js or get a CDN url for 3 versions of gothub." />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.13/semantic.min.css">
+<link rel="stylesheet" href="/css/main.css" />
+<link rel="stylesheet" href="/css/site.css" />
+<link rel="canonical" href="https://www.cdnpkg.com/gothub/file/index.js"> 
+
+
+
+</head>
+<body class="Site">
+
+	<div class="Site-header">
+						<div class="eyecatcher"></div>
+ <div class="ui stackable borderless main menu">
+ 
+    <div class="ui container">
+      <a href="/" class="header item">
+        <i class=""></i>&nbsp; CDNPKG .com
+      </a>
+      
+      <form class="header item" action="/-/search">
+	    <div class="ui icon large input">
+	      <input type="text" name="q" value="" placeholder="Search file, library...">
+	      <i class="search icon"></i>
+	    </div>
+	    <input type="hidden" name="t" value="lib">
+  	  </form>
+	  
+	  
+
+    </div>
+  </div>
+<div class="eyecatcherbottom"></div>				</div>
+	<div class="Site-content ui container">
+	
+			<div class="ui small breadcrumb"><a class="section" href="/">Home</a>
+<i class="right chevron icon divider"></i><a class="section" href="/gothub">gothub</a>
+<i class="right chevron icon divider"></i><div class="active section">index.js</div></div>		<script type="application/ld+json">{ "@context": "http://schema.org",
+  "@type": "BreadcrumbList",
+  "itemListElement": [{
+    "@type": "ListItem",
+    "position": 0,
+    "item": {
+      "@id": "/",
+      "name": "Home"
+    }
+  },
+{
+    "@type": "ListItem",
+    "position": 1,
+    "item": {
+      "@id": "/gothub",
+      "name": "gothub"
+    }
+  },
+{
+    "@type": "ListItem",
+    "position": 2,
+    "item": {
+      "@id": "null",
+      "name": "index.js"
+    }
+  }]}</script>		
+	 
+
+
+<h1 class="ui dividing header">index.js</h1>
+
+
+
+<table class="ui definition compact small table">
+  <tbody>
+    <tr>
+      <td>Filename</td>
+      <td>index.js</td>
+    </tr>
+        <tr>
+      <td>File Type</td>
+      <td>Javascript</td>
+    </tr>
+        <tr>
+      <td>Library</td>
+      <td><a href="/gothub" >gothub</a></td>
+    </tr>
+    <tr>
+      <td>Latest file version</td>
+      <td>1.0.2</td>
+    </tr>
+    <tr>
+      <td>Download latest file version</td>
+      <td><a target="_blank" rel="nofollow noopener noreferrer" href="https://get.cdnpkg.com/gothub/1.0.2/index.js">Download index.js latest version (1.0.2)</a></td>
+    </tr>
+</tbody></table>
+
+
+
+<h2 class="ui dividing header"><small>Download or get link</small></h2>
+<p>index.js is available in 3 versions of gothub.</p>
+
+<h3>1.0.2 <small><a title="Download index.js version 1.0.2" target="_blank" rel="nofollow noopener noreferrer" href="https://get.cdnpkg.com/gothub/1.0.2/index.js"><i class="download icon"></i></a></small></h3>
+<table class="ui definition small compact table">
+  <thead>
+    <tr><th></th>
+        <th>unpkg</th>
+      </tr></thead>
+  <tbody>
+  <!-- 
+    <tr>
+      <td>File size</td>
+             	<td>file size</td>
+    	     
+    </tr>
+ -->
+    <tr>
+      <td>URL</td>
+             	<td><a href="https://unpkg.com/gothub@1.0.2/index.js" target="_blank" rel="nofollow noopener noreferrer">https://unpkg.com/gothub@1.0.2/index.js</a></td>
+    	    </tr>
+        <tr>
+      <td>Html Snippet</td>
+             		
+    		    		<td><small>&lt;script type=&quot;text/javascript&quot; src=&quot;https://unpkg.com/gothub@1.0.2/index.js&quot;&gt;&lt;/script&gt;</small></td>
+    		    	    </tr>
+        <tr>
+      <td>Download</td>
+             	<td><a title="Download index.js version 1.0.2 from unpkg" href="#" onclick="if (!window.__cfRLUnblockHandlers) return false; javascript:downloadFile('https://unpkg.com/gothub@1.0.2/index.js','index.js');void(0)" data-cf-modified-2d745afde88cab5b12c5b7e1-=""><i class="download icon"></i>download</a></td>
+    	    </tr>
+</tbody></table>
+
+<h3>1.0.1 <small><a title="Download index.js version 1.0.1" target="_blank" rel="nofollow noopener noreferrer" href="https://get.cdnpkg.com/gothub/1.0.1/index.js"><i class="download icon"></i></a></small></h3>
+<table class="ui definition small compact table">
+  <thead>
+    <tr><th></th>
+        <th>unpkg</th>
+      </tr></thead>
+  <tbody>
+  <!-- 
+    <tr>
+      <td>File size</td>
+             	<td>file size</td>
+    	     
+    </tr>
+ -->
+    <tr>
+      <td>URL</td>
+             	<td><a href="https://unpkg.com/gothub@1.0.1/index.js" target="_blank" rel="nofollow noopener noreferrer">https://unpkg.com/gothub@1.0.1/index.js</a></td>
+    	    </tr>
+        <tr>
+      <td>Html Snippet</td>
+             		
+    		    		<td><small>&lt;script type=&quot;text/javascript&quot; src=&quot;https://unpkg.com/gothub@1.0.1/index.js&quot;&gt;&lt;/script&gt;</small></td>
+    		    	    </tr>
+        <tr>
+      <td>Download</td>
+             	<td><a title="Download index.js version 1.0.1 from unpkg" href="#" onclick="if (!window.__cfRLUnblockHandlers) return false; javascript:downloadFile('https://unpkg.com/gothub@1.0.1/index.js','index.js');void(0)" data-cf-modified-2d745afde88cab5b12c5b7e1-=""><i class="download icon"></i>download</a></td>
+    	    </tr>
+</tbody></table>
+
+<h3>1.0.0 <small><a title="Download index.js version 1.0.0" target="_blank" rel="nofollow noopener noreferrer" href="https://get.cdnpkg.com/gothub/1.0.0/index.js"><i class="download icon"></i></a></small></h3>
+<table class="ui definition small compact table">
+  <thead>
+    <tr><th></th>
+        <th>unpkg</th>
+      </tr></thead>
+  <tbody>
+  <!-- 
+    <tr>
+      <td>File size</td>
+             	<td>file size</td>
+    	     
+    </tr>
+ -->
+    <tr>
+      <td>URL</td>
+             	<td><a href="https://unpkg.com/gothub@1.0.0/index.js" target="_blank" rel="nofollow noopener noreferrer">https://unpkg.com/gothub@1.0.0/index.js</a></td>
+    	    </tr>
+        <tr>
+      <td>Html Snippet</td>
+             		
+    		    		<td><small>&lt;script type=&quot;text/javascript&quot; src=&quot;https://unpkg.com/gothub@1.0.0/index.js&quot;&gt;&lt;/script&gt;</small></td>
+    		    	    </tr>
+        <tr>
+      <td>Download</td>
+             	<td><a title="Download index.js version 1.0.0 from unpkg" href="#" onclick="if (!window.__cfRLUnblockHandlers) return false; javascript:downloadFile('https://unpkg.com/gothub@1.0.0/index.js','index.js');void(0)" data-cf-modified-2d745afde88cab5b12c5b7e1-=""><i class="download icon"></i>download</a></td>
+    	    </tr>
+</tbody></table>
+
+
+	</div>
+	
+	<div class="Site-footer">
+			<div class="ui inverted vertical footer segment" style="margin-top: 50px;">	
+			<div class="ui container stackable grid">
+				<div class="three wide column ">
+					<div class="ui inverted link list">
+						  	  <div class="item"><a href="/p/privacy-policy" >Privacy</a></div>
+	  	  					  <div class="item"><a href="/p/terms-conditions" >Terms</a></div>
+	  	  					  <div class="item"><a href="/p/contact" >Contact</a></div>
+					</div>				
+				</div>
+				<div class="three wide column ">
+					<div class="ui inverted link list">
+						  	  <div class="item"><a style="color:rgba(255,255,255,.9);" href="https://github.com/cdnpkg/cdnpkg.com" target="_blank"><i class="github icon"></i> Github</a></div>
+	  	  					  <div class="item"><a  style="color:rgba(255,255,255,.9);" href="https://twitter.com/cdnpkg" target="_blank"><i class="twitter icon"></i> Twitter</a></div>
+					</div>				
+				</div>							
+			</div>
+
+			<div class="ui container center aligned" >
+						<div id="footer"><small>Copyright 2022 - cdnpkg.com</small></div>
+			</div>
+		</div>
+		</div>
+
+
+	<script defer type="2d745afde88cab5b12c5b7e1-text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	 
+<script type="2d745afde88cab5b12c5b7e1-text/javascript">
+function downloadFile(url,filename){
+	var a = document.createElement('a');
+	a.href = url;
+	a.download = filename; // Set the file name.
+	a.style.display = 'none';
+	document.body.appendChild(a);
+	a.click();
+	delete a;
+}
+</script>
+<script src="/cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js" data-cf-settings="2d745afde88cab5b12c5b7e1-|49" defer></script></body>
+</html>
